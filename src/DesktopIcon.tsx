571e6f8993c92ffcd99df7f4b2ed945e72c99e65
GovernmentHack <32027859+GovernmentHack@users.noreply.github.com>
2023-10-21 12:01:34 -0500

    Added file names

--- src/DesktopIcon.tsx
+++ src/DesktopIcon.tsx
@@ -3,12 +3,49 @@ import { DragSourceMonitor, XYCoord, useDrag } from 'react-dnd'
 import styled from 'styled-components';
 import { DragTypes } from './utils/constants';
 
+const DRAG_OFFSET_FIX = 17;
+
+const IconContainer = styled.div`
+  height: 48px;
+  position: absolute;
+  width: 70px;
+  z-index: 99999;
+`;
+
 const IconImage = styled.div`
   background-repeat: no-repeat;
-  height: 100%;
-`
+  height: 32px;
+  margin-bottom: 4px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 32px;
+`;
 
-const DesktopIcon: React.FunctionComponent<{ icon: string, initialLocation?: XYCoord }> = ({icon, initialLocation = { x: 8, y: 8 }}) => {
+const IconText = styled.span`
+  color: white;
+  display: block;
+  font-family: px_sans_nouveaux;
+  font-size: 8px;
+  letter-spacing: -.025em;
+  line-height: 12px;
+  text-align: center;
+  text-rendering: optimizeLegibility;
+`;
+
+type DesktopIconProps = {
+  icon?: string;
+  initialLocation?: XYCoord;
+  fileName?: string;
+
+};
+
+const DesktopIcon: React.FunctionComponent<DesktopIconProps> = (
+  {
+    icon = "directory_closed_cool-0.png",
+    initialLocation = { x: 8, y: 8 },
+    fileName = "New Folder",
+  }
+) => {
   const [location, setLocation] = useState(initialLocation);
   const [{ isDragging, endLocation }, drag, dragPreview] = useDrag(() => ({
     type: DragTypes.DesktopIcon,
@@ -21,7 +58,7 @@ const DesktopIcon: React.FunctionComponent<{ icon: string, initialLocation?: XYC
     end: (draggedItem, monitor) => {
       const tempLocation = monitor.getDropResult() as XYCoord | null;
       if (tempLocation) {
-        setLocation(tempLocation);
+        setLocation({...tempLocation, x: tempLocation.x-DRAG_OFFSET_FIX });
       }
     },
     options: {
@@ -30,9 +67,10 @@ const DesktopIcon: React.FunctionComponent<{ icon: string, initialLocation?: XYC
   }));
 
   return (
-    <div ref={dragPreview} style={{ opacity: isDragging ? 0 : 1, left: location.x, top: location.y, position: "absolute", width: "48px", height: "48px", zIndex: 99999}}>
+    <IconContainer ref={dragPreview} style={{ opacity: isDragging ? 0 : 1, left: location.x, top: location.y}}>
       <IconImage ref={drag} style={{ backgroundImage: `url(icons/${icon})`}} />
-    </div>
+      <IconText>{fileName}</IconText>
+    </IconContainer>
   );
 }
 
