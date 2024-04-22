581d83e8345a5db9e4603443036c477b9ccb4585
GovernmentHack <32027859+GovernmentHack@users.noreply.github.com>
2024-04-22 18:47:18 -0500

    Add newMenuButton tests

--- src/Components/ContextMenu/NewMenuButton.dom.test.tsx
+++ src/Components/ContextMenu/NewMenuButton.dom.test.tsx
@@ -3,8 +3,14 @@ import { render, fireEvent } from "@testing-library/react";
 import React from "react";
 import { FileContext } from "../../App";
 import { NewMenuButton } from "./NewMenuButton";
+import * as clickHandlerModule from "./getNewButtonClickHandler";
 
 const mockSetWindowNewMenuIsOpen = vi.fn();
+
+const mockNewButtonClickHandler = vi.fn();
+vi.spyOn(clickHandlerModule, "getNewButtonClickHandler").mockReturnValue(
+  mockNewButtonClickHandler,
+);
 
 describe("NewMenuButton", () => {
   beforeEach(() => {
@@ -62,5 +68,59 @@ describe("NewMenuButton", () => {
     expect(() =>
       getByTestId("context_menu_new_textfile_button"),
     ).toThrowError();
+  });
+
+  it("executes the provided click handler when new folder is clicked", () => {
+    const { getByTestId } = render(
+      <FileContext.Provider
+        value={{
+          files: [],
+          setFiles: vi.fn(),
+          loading: false,
+          setLoading: vi.fn(),
+        }}
+      >
+        <NewMenuButton
+          fileId={null}
+          windowContextMenuLocation={{ x: 1, y: 1 }}
+          setWindowContextMenuIsOpen={mockSetWindowNewMenuIsOpen}
+        />
+      </FileContext.Provider>,
+    );
+
+    const newMenuButton = getByTestId("context_menu_new_button");
+    fireEvent.mouseEnter(newMenuButton);
+
+    const newFolderButton = getByTestId("context_menu_new_folder_button");
+    fireEvent.click(newFolderButton);
+
+    expect(mockNewButtonClickHandler).toBeCalledTimes(1);
+  });
+
+  it("executes the provided click handler when new text file is clicked", () => {
+    const { getByTestId } = render(
+      <FileContext.Provider
+        value={{
+          files: [],
+          setFiles: vi.fn(),
+          loading: false,
+          setLoading: vi.fn(),
+        }}
+      >
+        <NewMenuButton
+          fileId={null}
+          windowContextMenuLocation={{ x: 1, y: 1 }}
+          setWindowContextMenuIsOpen={mockSetWindowNewMenuIsOpen}
+        />
+      </FileContext.Provider>,
+    );
+
+    const newMenuButton = getByTestId("context_menu_new_button");
+    fireEvent.mouseEnter(newMenuButton);
+
+    const newTextFileButton = getByTestId("context_menu_new_textfile_button");
+    fireEvent.click(newTextFileButton);
+
+    expect(mockNewButtonClickHandler).toBeCalledTimes(1);
   });
 });
